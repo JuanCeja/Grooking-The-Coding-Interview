a7e90f32e1901da782c30d4d44c1f020ef38962c
JuanCeja <jmc347ls2@yahoo.com>
2024-01-15 21:35:15 -0800

    initial commit for containsDuplicate algorithm

---
+++ warmUp/easy/containsDuplicate.js
@@ -0,0 +1,16 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+// Input: nums= [1, 2, 3, 4]
+// Output: false - There are no duplicates in the given array.
+
+// Example 2:
+// Input: nums= [1, 2, 3, 1]
+// Output: true - '1' is repeating.
+
+function containsDuplicate(nums) {
+    // TODO: Write your code here
+    return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1])); // true
